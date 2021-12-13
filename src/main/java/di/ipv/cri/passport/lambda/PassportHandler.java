47aaac92133623542e0b2e086a458b9db0a99753
SunilDGDS <sunil.dussoye@digital.cabinet-office.gov.uk>
2021-12-13 12:48:21 +0000

    Base lambda version for passporthandler

---
+++ src/main/java/di/ipv/cri/passport/lambda/PassportHandler.java
@@ -0,0 +1,19 @@
+package di.ipv.cri.passport.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import di.ipv.cri.passport.helpers.ApiGatewayResponseGenerator;
+import org.apache.http.HttpStatus;
+
+public class PassportHandler
+        implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent input, Context context) {
+
+        return ApiGatewayResponseGenerator.proxyJsonResponse(HttpStatus.SC_OK, "Hello world");
+    }
+}
