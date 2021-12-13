c4bf243e361c08dbeb04f8edf7e1f47015babff5
Daniel Pomfret <daniel.pomfret@digital.cabinet-office.gov.uk>
2021-12-13 11:11:33 +0000

    PYI-446: Add Terraform to create passport lamdba endpoint
    Co-authored-by: Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>

---
+++ terraform/lambda/passport.tf
@@ -0,0 +1,14 @@
+module "passport" {
+  source      = "../modules/endpoint"
+  environment = var.environment
+
+  rest_api_id            = aws_api_gateway_rest_api.ipv_cri_uk_passport.id
+  rest_api_execution_arn = aws_api_gateway_rest_api.ipv_cri_uk_passport.execution_arn
+  root_resource_id       = aws_api_gateway_rest_api.ipv_cri_uk_passport.root_resource_id
+  http_method            = "POST"
+  path_part              = "passport"
+  handler                = "uk.gov.di.ipv.lambda.PassportHandler::handleRequest"
+  function_name          = "${var.environment}-passport"
+  role_name              = "${var.environment}-passport-role"
+}
+
