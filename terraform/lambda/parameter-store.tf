997ca8cf32783511893f2634af4303372cf82345
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2021-12-16 12:46:28 +0000

    PYI-465: Give parameters a value
    The parameter resources can't have a empty string value.

--- terraform/lambda/parameter-store.tf
+++ terraform/lambda/parameter-store.tf
@@ -2,7 +2,7 @@ resource "aws_ssm_parameter" "passport_tls_key" {
   name        = "/${var.environment}/cri/passport/tls-key"
   description = "The TLS key used by the passport CRI"
   type        = "SecureString"
-  value       = "" # Will be set by hand
+  value       = "." # Will be set by hand
   overwrite   = false
 }
 
@@ -10,7 +10,7 @@ resource "aws_ssm_parameter" "passport_tls_cert" {
   name        = "/${var.environment}/cri/passport/tls-cert"
   description = "The TLS certificate used by the passport CRI"
   type        = "String"
-  value       = "" # Will be set by hand after a cert has been issued
+  value       = "." # Will be set by hand after a cert has been issued
   overwrite   = false
 }
 
@@ -18,7 +18,7 @@ resource "aws_ssm_parameter" "passport_signing_cert" {
   name        = "/${var.environment}/cri/passport/signing-cert"
   description = "The signing certificate used by the passport CRI"
   type        = "String"
-  value       = "" # Will be set by hand after a cert has been issued
+  value       = "." # Will be set by hand after a cert has been issued
   overwrite   = false
 }
 
@@ -26,7 +26,7 @@ resource "aws_ssm_parameter" "passport_encryption_cert" {
   name        = "/${var.environment}/cri/passport/signing-cert"
   description = "The encryption certificate used by the passport CRI"
   type        = "String"
-  value       = "" # Will be set by hand after a cert has been issued
+  value       = "." # Will be set by hand after a cert has been issued
   overwrite   = false
 }
 
