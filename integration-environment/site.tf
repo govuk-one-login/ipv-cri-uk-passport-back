e1dafbbeeb245ea79751f518ee52fc20c42edb69
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-01-13 08:55:51 +0000

    PYI-492: Set up an environment for running integration tests
    This wraps the intTest gradle task with tasks to start up and tear down
    an environment for running integration tests in.
    
    It starts a localstack container and applies specific pieces of
    Terraform into it that are needed for integration tests. At the moment
    it's just dynamoDB tables.
    
    This is good as it means we're running the tests against infrastructure
    created with the same Terraform we're using in prod.
    
    It would be nice to be able to tear it all down and set it up again
    between tests, but this won't do that...

---
+++ integration-environment/site.tf
@@ -0,0 +1,46 @@
+# For setting up aws infrastructure for integration tests
+
+terraform {
+  required_version = "1.0.9"
+
+  backend "local" {}
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.66"
+    }
+  }
+}
+
+provider "aws" {
+  access_key                  = "mock_access_key"
+  region                      = "eu-west-2"
+  s3_force_path_style         = true
+  secret_key                  = "mock_secret_key"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    dynamodb   = "http://0.0.0.0:4566"
+  }
+}
+
+module "cri-passport-lambda" {
+  source                             = "../terraform/lambda"
+  environment                        = "integration-test"
+  use_localstack                     = true
+  dcs_tls_root_cert                  = ""
+  passport_tls_cert                  = ""
+  local_only_passport_tls_key        = ""
+  passport_signing_cert              = ""
+  local_only_passport_signing_key    = ""
+  passport_encryption_cert           = ""
+  local_only_passport_encryption_key = ""
+  dcs_signing_cert                   = ""
+  stub_dcs_signing_key               = ""
+  dcs_encryption_cert                = ""
+  stub_dcs_encryption_key            = ""
+  dcs_tls_intermediate_cert          = ""
+}
--- src/integration-test/java/uk/gov/di/ipv/cri/passport/integrationtest/DataStoreIT.java
+++ src/integration-test/java/uk/gov/di/ipv/cri/passport/integrationtest/DataStoreIT.java
@@ -7,11 +7,15 @@ import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import uk.gov.di.ipv.cri.passport.domain.DcsResponse;
 import uk.gov.di.ipv.cri.passport.domain.PassportFormRequest;
 import uk.gov.di.ipv.cri.passport.persistence.DataStore;
 import uk.gov.di.ipv.cri.passport.persistence.item.AuthorizationCodeItem;
 import uk.gov.di.ipv.cri.passport.persistence.item.PassportCheckDao;
+import uk.org.webcompere.systemstubs.environment.EnvironmentVariables;
+import uk.org.webcompere.systemstubs.jupiter.SystemStub;
+import uk.org.webcompere.systemstubs.jupiter.SystemStubsExtension;
 
 import java.net.URI;
 import java.time.LocalDate;
@@ -21,12 +25,14 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@ExtendWith(SystemStubsExtension.class)
 public class DataStoreIT {
 
-    private static final String dynamodbUrl = "http://localhost:4567";
+    private static final String dynamodbUrl = "http://localhost:4566";
     private static final URI endpoint = URI.create(dynamodbUrl);
-    private static final String DCS_RESPONSE_TABLE_NAME = "local-dev-dcs-response";
-    private static final String DCS_AUTHORIZATION_TABLE_NAME = "local-dev-auth-codes";
+    private static final String DCS_RESPONSE_TABLE_NAME = "integration-test-dcs-response";
+    private static final String DCS_AUTHORIZATION_TABLE_NAME =
+            "integration-test-cri-passport-auth-codes";
     private static final DataStore<PassportCheckDao> dcsResponseDataStore =
             new DataStore<>(
                     DCS_RESPONSE_TABLE_NAME, PassportCheckDao.class, DataStore.getClient(endpoint));
@@ -41,6 +47,12 @@ public class DataStoreIT {
             localDynamoDb.getTable(DCS_RESPONSE_TABLE_NAME);
     private static final Table authorizationCodeTableHarness =
             localDynamoDb.getTable(DCS_AUTHORIZATION_TABLE_NAME);
+
+    @SystemStub
+    private final EnvironmentVariables environmentVariables =
+            new EnvironmentVariables(
+                    "AWS_ACCESS_KEY_ID", "ASDFGHJKL",
+                    "AWS_SECRET_ACCESS_KEY", "1234567890987654321");
 
     @Test
     public void shouldPutPassportCheckIntoTable() {
