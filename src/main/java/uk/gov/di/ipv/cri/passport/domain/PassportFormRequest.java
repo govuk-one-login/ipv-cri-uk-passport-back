a3e26e9d1890bf614ed4ba7716c65c3d8f2c02ee
Danny Hayes <daniel.hayes@digital.cabinet-office.gov.uk>
2022-01-14 14:39:40 +0000

    PYI-452: Fix timestamp field date format
    Co-authored-by: Dan Worth <dan.worth@digital.cabinet-office.gov.uk>
    Co-authored-by: Daniel Pomfret <daniel.pomfret@digital.cabinet-office.gov.uk>

--- src/main/java/uk/gov/di/ipv/cri/passport/domain/PassportFormRequest.java
+++ src/main/java/uk/gov/di/ipv/cri/passport/domain/PassportFormRequest.java
@@ -5,15 +5,16 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.UUID;
 
 @DynamoDbBean
 public class PassportFormRequest {
     private static final String DATE_FORMAT = "yyyy-MM-dd";
+    private static final String TIMESTAMP_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+
     private static final String TIME_ZONE = "UTC";
 
     @JsonProperty private UUID correlationId;
@@ -47,7 +48,7 @@ public class PassportFormRequest {
         this.expiryDate = expiryDate;
         this.correlationId = UUID.randomUUID();
         this.requestId = UUID.randomUUID();
-        this.timestamp = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
+        this.timestamp = new SimpleDateFormat(TIMESTAMP_DATE_FORMAT).format(new Date());
     }
 
     public UUID getCorrelationId() {
