c4589413f1951a3abecb4c7f0fa53a9e18880897
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2022-04-06 09:50:00 +0100

    PYIC-870: Fix code smells

--- lib/src/main/java/uk/gov/di/ipv/cri/passport/library/domain/verifiablecredential/VerifiableCredentialConstants.java
+++ lib/src/main/java/uk/gov/di/ipv/cri/passport/library/domain/verifiablecredential/VerifiableCredentialConstants.java
@@ -1,19 +1,19 @@
 package uk.gov.di.ipv.cri.passport.library.domain.verifiablecredential;
 
 public class VerifiableCredentialConstants {
-    public static String VC_CONTEXT = "@context";
-    public static String W3_BASE_CONTEXT = "https://www.w3.org/2018/credentials/v1";
-    public static String DI_CONTEXT =
+    public static final String VC_CONTEXT = "@context";
+    public static final String W3_BASE_CONTEXT = "https://www.w3.org/2018/credentials/v1";
+    public static final String DI_CONTEXT =
             "https://vocab.london.cloudapps.digital/contexts/identity-v1.jsonld";
-    public static String VC_TYPE = "type";
-    public static String VERIFIABLE_CREDENTIAL_TYPE = "VerifiableCredential";
-    public static String IDENTITY_CHECK_CREDENTIAL_TYPE = "IdentityCheckCredential";
-    public static String CREDENTIAL_SUBJECT_NAME = "name";
-    public static String CREDENTIAL_SUBJECT_BIRTH_DATE = "birthDate";
-    public static String CREDENTIAL_SUBJECT_ADDRESS = "address";
-    public static String VC_CREDENTIAL_SUBJECT = "credentialSubject";
-    public static String VC_EVIDENCE = "evidence";
-    public static String VC_CLAIM = "vc";
+    public static final String VC_TYPE = "type";
+    public static final String VERIFIABLE_CREDENTIAL_TYPE = "VerifiableCredential";
+    public static final String IDENTITY_CHECK_CREDENTIAL_TYPE = "IdentityCheckCredential";
+    public static final String CREDENTIAL_SUBJECT_NAME = "name";
+    public static final String CREDENTIAL_SUBJECT_BIRTH_DATE = "birthDate";
+    public static final String CREDENTIAL_SUBJECT_ADDRESS = "address";
+    public static final String VC_CREDENTIAL_SUBJECT = "credentialSubject";
+    public static final String VC_EVIDENCE = "evidence";
+    public static final String VC_CLAIM = "vc";
 
     private VerifiableCredentialConstants() {}
 }
