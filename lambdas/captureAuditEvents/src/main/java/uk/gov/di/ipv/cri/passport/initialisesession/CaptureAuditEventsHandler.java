9aafa093953b88122164ddfba261ab68bd645c06
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2023-07-05 11:42:54 +0100

    LIME-706: Fixed spotless and checkov

--- lambdas/captureAuditEvents/src/main/java/uk/gov/di/ipv/cri/passport/initialisesession/CaptureAuditEventsHandler.java
+++ lambdas/captureAuditEvents/src/main/java/uk/gov/di/ipv/cri/passport/initialisesession/CaptureAuditEventsHandler.java
@@ -10,22 +10,22 @@ import software.amazon.lambda.powertools.logging.Logging;
 import software.amazon.lambda.powertools.metrics.Metrics;
 import uk.gov.di.ipv.cri.passport.library.helpers.LogHelper;
 
-
 public class CaptureAuditEventsHandler implements RequestHandler<SQSEvent, Void> {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public CaptureAuditEventsHandler() {
-    }
+    public CaptureAuditEventsHandler() {}
 
     @Override
     @Logging(clearState = true, correlationIdPath = CorrelationIdPathConstants.API_GATEWAY_REST)
     @Metrics(captureColdStart = true)
-    public Void handleRequest(
-            SQSEvent input, Context context) {
+    public Void handleRequest(SQSEvent input, Context context) {
         LogHelper.attachComponentIdToLogs();
         try {
-            for(SQSEvent.SQSMessage msg : input.getRecords()){
-                LOGGER.info("Audit event consumed", msg.getEventSource(), "messageBody: " + msg.getBody());
+            for (SQSEvent.SQSMessage msg : input.getRecords()) {
+                LOGGER.info(
+                        "Audit event consumed",
+                        msg.getEventSource(),
+                        "messageBody: " + msg.getBody());
             }
             return null;
 
